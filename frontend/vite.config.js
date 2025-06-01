b311d1ba5b9d956a3aa03152172dc4a8c2a6dff9
Sweetreams <pashasurov12345@gmail.com>
2025-06-01 14:11:33 +0700

    вапыв

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  }
 })
