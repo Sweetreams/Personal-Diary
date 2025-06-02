08478a2a096ea8ba63b02abbd6dab9f8976e3ea2
Sweetreams <pashasurov12345@gmail.com>
2025-06-02 13:03:37 +0700

    fgdf

--- frontend/src/components/customList/ListSideBarTags.jsx
+++ frontend/src/components/customList/ListSideBarTags.jsx
@@ -7,11 +7,11 @@ const ListSideBarTags = ({ props }) => {
     const color = el.color
     return (
       <Tooltip title={el.tag}>
-        <a style={{ display: "flex", flexDirection: "row", alignItems: "center", background: color, padding: "2px 5px", borderRadius: 8, minWidth: "60px", maxWidth: "50%", fontSize: 14, color: "#333333" }}>
-          <div style={{ background: "#fff", position: "relative", borderRadius: 20, width: "17px", height: "17px", marginRight: 7 }}>
+        <a className="listSideBarTagsLink" style={{background: color}}>
+          <div className="listSideBarTagsLinkContainer" >
             <span style={{ position: "absolute", left: 6, top: 1.5 }}>{el.count}</span>
           </div>
-          <span style={{width: "50%", textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden", position: "relative", top: 1, color: Luma(HEXtoRGB(el.color)) > 50 ? "#333" : "#fff" }}>{el.tag}</span>
+          <span className="listSideBarTagsLinkName" style={{color: Luma(HEXtoRGB(el.color)) > 50 ? "#333" : "#fff" }}>{el.tag}</span>
         </a>
       </Tooltip>
     )
